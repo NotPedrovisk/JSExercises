6ff796bda6d06c3ab1bce0718ee8534f961ad616
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-03-11 22:56:09 -0400

    Finished exercise#2 with no mistakes, had to look back to lesson materials for help

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let message = ""
+    
+    if(num <0){
+        return "ERROR"
+    }
+
+    for (let i = 0; i < num; i++){
+        if (num > 0){
+            message += string
+        }
+    }
+    
+    return message
 
 };
 
