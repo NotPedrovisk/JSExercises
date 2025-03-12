580fda02a415940948ab53f12feb829dbd7b675d
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-03-11 23:42:09 -0400

    finished exercise#3, thought it was an loop problem, had to do research, switched to array, but was 75% there, just missing the join() part

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    const msgArray = string.split();
+    let reversedMsg = msgArray.reverse().join("");
+    
+    return reversedMsg
 
+    
 };
 
 // Do not edit below this line
