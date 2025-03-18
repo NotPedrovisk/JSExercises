1e2e5737c3169daaaf4926c8f91b5b84885248d7
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-03-18 01:31:55 -0400

    found solution to first test of exercise#4, had to do a bit o research but came up with my own code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(currentArray, toRemove) {
+
+      //for loop maybe
+      //for every item in array, check if it's the same as number to remove
+      //if it is, remove number and return new array
+    const beforeArray = currentArray
+    const removable = toRemove
+
+    //for every item in the array
+    for(number of beforeArray){
+
+        //if the current cheched item is equal to the variable passed on the function
+        if(number == removable){
+
+            //gets the index of the item to remove
+            numberIndex = beforeArray.indexOf(number);
+            //splice removes one item at the index of the item to remove, removing the item from array
+            beforeArray.splice(numberIndex, 1)
+            return beforeArray
+        }
+    }
+
 };
 
 // Do not edit below this line
