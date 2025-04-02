63781519d857d53b35578e7f50d8b85c2531645e
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-04-02 14:44:49 -0400

    finished exercise 5, did a bit different from solution, but still readable and still works

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    let minNumber = Math.min(num1,num2)
+    let maxNumber = Math.max(num1,num2)
+
+    let total = 0
+    if((typeof (num1 && num2) != "number")||((num1 || num2)< 0)||((num1 || num2)%1 !=0)){
+        return "ERROR"
+        
+    }
+    else{
+        
+        for(let n = minNumber; n < maxNumber+1; n++){
+                total += n
+            }
+    }
+    
+    console.log(minNumber, maxNumber)
+    return total
 };
 
 // Do not edit below this line
