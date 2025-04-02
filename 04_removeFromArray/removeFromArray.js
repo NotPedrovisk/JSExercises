6e4c89bac7a1935215f8c48a8d4270d179fc9d22
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-04-02 00:11:24 -0400

    finished exercise 4 after much researching, was trying to do a loop instead of using filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,26 +1,13 @@
-const removeFromArray = function(currentArray, toRemove) {
+const removeFromArray = function(currentArray, ...toRemove) {
 
-      //for loop maybe
-      //for every item in array, check if it's the same as number to remove
-      //if it is, remove number and return new array
-    const beforeArray = currentArray
-    const removable = toRemove
+let array = (currentArray);
 
-    //for every item in the array
-    for(number of beforeArray){
+let newArray = array.filter(n =>!toRemove.includes(n))
 
-        //if the current cheched item is equal to the variable passed on the function
-        if(number == removable){
 
-            //gets the index of the item to remove
-            numberIndex = beforeArray.indexOf(number);
-            //splice removes one item at the index of the item to remove, removing the item from array
-            beforeArray.splice(numberIndex, 1)
-            return beforeArray
-        }
-    }
-
+return newArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
