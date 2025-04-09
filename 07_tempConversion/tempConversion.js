6ab72a6e4e301eacd0ee85a32044525d93457095
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-04-08 21:56:04 -0400

    finished exercise 7 easily

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let convertedCel = (fah - 32) * 5/9;
+  let roundedCel = Math.round(convertedCel * 10)/10
+  return roundedCel
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let convertedFah = ((cel * 9/5) + 32);
+  let roundedFah = Math.round(convertedFah * 10)/10
+  return roundedFah
 };
 
 // Do not edit below this line
