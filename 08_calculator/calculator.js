c1daa5e6603d9cbc4981993c7090b29a5658d20c
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-07-04 01:43:16 -0400

    finished sums, didnt understand at first but read over past lessons and understood it

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,9 @@ const subtract = function(a,b) {
 	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((initial, currentItem)=>
+    initial + currentItem,0)
 };
 
 const multiply = function() {
