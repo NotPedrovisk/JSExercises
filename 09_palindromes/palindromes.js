71dfe02208dfa0dffc1273423f3de4b71fbb3328
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-07-14 21:52:46 -0400

    had no idea what to do, looked at results and tried to find how it worked out

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    const ALPHANUMERICAL = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-};
+    let filteredString = arr.toLowerCase()
+    .split("")
+    .filter((character)=>ALPHANUMERICAL.includes(character))
+    .join("");
+    
+
+    console.log(filteredString);
+
+    let reversedString = filteredString.split("")
+    .reverse()
+    .join("");
+
+    return filteredString === reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
