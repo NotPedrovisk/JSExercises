f7017807b6304ad122e9662da978b1318ce5add1
NotPedrovisk <not.pedrovisk69@gmail.com>
2025-07-22 20:51:23 -0400

    finished exercise, had to look at result but studied it over, then commented for future understanding

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,12 +1,29 @@
+//calculates age when arguments are given
 const getAge = function(birth,death){
-
-    
+    //if no deathdate argument if provided, will use current date as death
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    //subtracts birthdate from deathdate to get age
     return (death - birth)
 }
 
 
-const findTheOldest = function(arr) {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest,current)=>{
+        //uses getAge to calculate first persons age, that person will become the oldest
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        //on second iteration, will check second person(current)
+        //if current persons age is older than oldest, current will become the oldest then will iterate through 
+        //array until done
+        if(oldestAge < currentAge){
+            return current;
+        } else{
+            return oldest;
+        };
+        
+    })
 }
 
 
